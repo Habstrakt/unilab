85821c5211c960c9cdc89496fdaa92e9ddb41c12
Pavel Gribanov <iamhab@gmail.com>
2024-08-05 16:34:40 +1000

    change all test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,46 +1,82 @@
 import {test, expect} from "@playwright/test";
+import { describe } from "node:test";
 import { qase } from 'playwright-qase-reporter';
+import axios from "axios";
 
-import { login, openNewTab } from "./utils";
+describe("Убедиться, что на странице смены пароля присутствуют необходимые поля ввода и кнопки для смены пароля.", () => {
+    test("Наличие полей ввода и кнопок для смены пароля", async ({browser}) => {
+        qase.id(3);
+        qase.title("Наличие полей ввода и кнопок для смены пароля");
+
+        const context = await browser.newContext();
+        const page = await context.newPage();
+
+        const url1 = "http://10.25.1.88:8005/login";
+        const url2 = "http://vlgwkcontrol.no-ip.org:38190/login/";
+        let urlToUse:string;
+
+        try {
+            const response1 = await axios.get(url1);
+            if (response1.status >= 200 && response1.status < 400) {
+                urlToUse = url1;
+            } else {
+                throw new Error(`Ошибка: статус-код первого URL: ${response1.status}`);
+            }
+            } catch (error) {
+                console.error(error);
+                urlToUse = url2;
+            };
 
 
-test("Наличие полей ввода и кнопок для смены пароля", async ({ browser }) => {
-    qase.id(3);
-    qase.title("MIS-3: Наличие полей ввода и кнопок для смены пароля");
 
-    const context = await browser.newContext();
-    const page = await context.newPage();
+        await page.goto(urlToUse);
 
-    await test.step("Step 1", async () => {
-        await login(page, "test3_ttt", "uni_987572*msa");
+        await page.locator("#id_username").fill("test3_ttt");
+        await page.locator("#id_password").fill("uni_987572*msa");
+        await page.locator("#login-form-content-main > form > button").click();
+
+        await test.step("Step 1", async() => {
+            await expect(page.locator("#breadcrumbs-container")).toContainText("Главная страница");
+        });
+
+        await test.step("Step 2", async() => {
+            const menuSelector = page.locator("#user-menu");
+
+            await expect(menuSelector).toBeVisible();
+            await menuSelector.click();
+        });
+
+        await test.step("Step 3", async() => {
+            const linkSelector = page.locator("#user-menu-block > ul > li:nth-child(1) > a");
+
+            await expect(linkSelector).toBeVisible();
+            await linkSelector.click();
+        });
+
+        const newTab = await context.waitForEvent("page");
+
+        await test.step("Step 4", async() => {
+            await expect(newTab.locator(".header")).toContainText("Изменение своего пароля");
+        });
+
+        await test.step("Step 5", async() => {
+            await expect(newTab.locator("#id_old_password")).toBeVisible();
+        });
+
+        await test.step("Step 6", async() => {
+            await expect(newTab.locator("#id_new_password1")).toBeVisible();
+        });
+
+        await test.step("Step 7", async() => {
+            await expect(newTab.locator("#id_new_password2")).toBeVisible();
+        });
+
+        await test.step("Step 8 ", async() => {
+            await expect(newTab.locator("#container > form > div.button-place.sticky > div > button.btn-cancel")).toBeVisible();
+        });
+
+        await test.step("Step 9", async() => {
+            await expect(newTab.locator("#container > form > div.button-place.sticky > button.btn-ok")).toBeVisible();
+        });
     });
-
-    //  Step #4
-    const newTab = await openNewTab(context, page, "#user-menu", "#user-menu-block > ul > li:nth-child(1) > a", "Изменение своего пароля");
-
-
-
-    const oldPassword = newTab.locator("#id_old_password");
-    const newPassword = newTab.locator("#id_new_password1");
-    const confirmNewPassword = newTab.locator("#id_new_password2");
-
-    await test.step("Step 5", async() => {
-        await expect(oldPassword).toBeVisible();
-    })
-
-    await test.step("Step 6", async() => {
-        await expect(newPassword).toBeVisible();
-    })
-
-    await test.step("Step 7", async() => {
-        await expect(confirmNewPassword).toBeVisible();
-    })
-
-    await test.step("Step 8", async() => {
-        await expect(newTab.locator("#container > form > div.button-place.sticky > div > button.btn-cancel")).toBeVisible();
-    })
-
-    await test.step("Step 8", async() => {
-        await expect(newTab.locator("#container > form > div.button-place.sticky > button.btn-ok")).toBeVisible();
-    })
 });
